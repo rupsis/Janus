1e64db46ae53f6160e49aa9af95cb972e8f376cc
Nate Rupsis <nrupsis@gmail.com>
2024-02-07 10:16:25 -0600

    finish chapter 1, got basic event handlers in place

--- window/Window.cpp
+++ window/Window.cpp
@@ -19,13 +19,52 @@ bool Window::init(unsigned int width, unsigned int height, std::string title) {
     return false;
   }
 
+  /* 1) save the pointer to the instance as user pointer */
+  glfwSetWindowUserPointer(mWindow, this);
+
+  /* 2) use a lambda to get the pointer and call the member function */
+  glfwSetWindowCloseCallback(mWindow, [](GLFWwindow *win) {
+    auto thisWindow = static_cast<Window *>(glfwGetWindowUserPointer(win));
+    thisWindow->handleWindowCloseEvents();
+  });
+
+  glfwSetWindowPosCallback(mWindow, [](GLFWwindow *win, int xpos, int ypos) {
+    auto thisWindow = static_cast<Window *>(glfwGetWindowUserPointer(win));
+    thisWindow->handleWindowMoveEvents(xpos, ypos);
+  });
+
+  glfwSetKeyCallback(mWindow, [](GLFWwindow *win, int key, int scancode, int action, int mods) {
+    auto thisWindow = static_cast<Window *>(glfwGetWindowUserPointer(win));
+    thisWindow->handleKeyEvents(key, scancode, action, mods);
+  });
+
+  glfwSetMouseButtonCallback(mWindow, [](GLFWwindow *win, int button, int action, int mods) {
+    auto thisWindow = static_cast<Window *>(glfwGetWindowUserPointer(win));
+    thisWindow->handleMouseButtonEvents(button, action, mods);
+  });
+
+  glfwMakeContextCurrent(mWindow);
+
   Logger::log(1, "%s: Window successfully initialized\n", __FUNCTION__);
 
   return true;
 }
 
 void Window::mainLoop() {
+  // Wait for vertical sync, else flickering / tearing might occur.
+  glfwSwapInterval(1);
+  float color = 0.0f;
+
   while (!glfwWindowShouldClose(mWindow)) {
+    color >= 1.0f ? color = 0.0f : color += 0.01f;
+    glClearColor(color, color, color, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // By default GLFW activates double buffering
+    //  next render call is in back buffer, previous render frame
+    // is in front buffer. Swap them.
+    glfwSwapBuffers(mWindow);
+
     // Poll events in main loop.
     glfwPollEvents();
   }
@@ -35,4 +74,82 @@ void Window::cleanup() {
   Logger::log(1, "%s: Terminating Window\n", __FUNCTION__);
   glfwDestroyWindow(mWindow);
   glfwTerminate();
+}
+
+/** Window event handlers. */
+void Window::handleWindowCloseEvents() {
+  Logger::log(1, "%s: Window got close event... bye!\n", __FUNCTION__);
+}
+
+void Window::handleWindowMoveEvents(int xpos, int ypos) {
+  Logger::log(1, "%s: Window updated to (%i,%i)\n", __FUNCTION__, xpos, ypos);
+}
+
+/** Keyboard event handler. */
+void Window::handleKeyEvents(int key, int scancode, int action, int mods) {
+  std::string actionName;
+
+  switch (action) {
+    case GLFW_PRESS:
+      actionName = "pressed";
+      break;
+    case GLFW_RELEASE:
+      actionName = "released";
+      break;
+    case GLFW_REPEAT:
+      actionName = "repeated";
+      break;
+    default:
+      actionName = "invalid";
+      break;
+  }
+
+  const char *keyName = glfwGetKeyName(key, 0);
+  Logger::log(1,
+              "%s: key %s (key %i, scancode %i) %s\n",
+              __FUNCTION__,
+              keyName,
+              key,
+              scancode,
+              actionName.c_str());
+}
+
+/** Mouse button events handlers. */
+void Window::handleMouseButtonEvents(int button, int action, int mods) {
+  std::string actionName;
+
+  switch (action) {
+    case GLFW_PRESS:
+      actionName = "pressed";
+      break;
+    case GLFW_RELEASE:
+      actionName = "released";
+      break;
+    default:
+      actionName = "invalid";
+      break;
+  }
+
+  std::string mouseButtonName;
+  switch (button) {
+    case GLFW_MOUSE_BUTTON_LEFT:
+      mouseButtonName = "left";
+      break;
+    case GLFW_MOUSE_BUTTON_MIDDLE:
+      mouseButtonName = "middle";
+      break;
+    case GLFW_MOUSE_BUTTON_RIGHT:
+      mouseButtonName = "right";
+      break;
+    default:
+      mouseButtonName = "other";
+      break;
+  }
+
+  Logger::log(1,
+              "%s: %s mouse button (%i) %s\n",
+              __FUNCTION__,
+              mouseButtonName.c_str(),
+              button,
+              actionName.c_str());
 }
--- window/Window.h
+++ window/Window.h
@@ -1,13 +1,23 @@
 #pragma once
-#include <string>
 #include <GLFW/glfw3.h>
+#include <string>
 
-class Window{
-    public:
-    bool init(unsigned int width, unsigned int height, std::string title);
-    void mainLoop();
-    void cleanup();
+class Window {
+ public:
+  bool init(unsigned int width, unsigned int height, std::string title);
+  void mainLoop();
+  void cleanup();
 
-    private:
-        GLFWwindow *mWindow = nullptr;
+  /** Keyboard event handler. */
+  void handleKeyEvents(int key, int scancode, int action, int mods);
+
+ private:
+  GLFWwindow *mWindow = nullptr;
+
+  /** Window event handlers. */
+  void handleWindowCloseEvents();
+  void handleWindowMoveEvents(int xpos, int ypos);
+
+  /** Mouse event handler.*/
+  void handleMouseButtonEvents(int button, int action, int mods);
 };
