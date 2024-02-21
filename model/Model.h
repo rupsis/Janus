4f134e08e34b95683505219468d9b8f14fad2cd8
Nate Rupsis <nrupsis@gmail.com>
2024-02-20 23:51:48 -0500

    fixing index out of bounds issue

--- model/Model.h
+++ model/Model.h
@@ -1,13 +1,15 @@
 #pragma once
-#include "OGLRenderData.h"
 #include <glm/glm.hpp>
 #include <vector>
+
+#include "OGLRenderData.h"
 
 class Model {
  public:
   void init();
+
   OGLMesh getVertexData();
 
  private:
-  OGLMesh mVertexData{};
+  OGLMesh mVertexData;
 };
