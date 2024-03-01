59d42b58c2899fac4371066c074eda8c79bd618c
Nate Rupsis <nrupsis@gmail.com>
2024-02-29 22:44:46 -0500

    remove model rotation

--- opengl/OGLRenderer.cpp
+++ opengl/OGLRenderer.cpp
@@ -101,21 +101,16 @@ void OGLRenderer::draw() {
                                        0.1f,
                                        100.f);
 
-  float t = glfwGetTime();
-
   glm::mat4 model = glm::mat4(1.0);
 
   // draw triangle from buffer
   if (mRenderData.rdUseChangedShader) {
-    glm::vec3 offset = glm::vec3(0.0, 0.0, -1.0);
-    model = glm::rotate(glm::mat4(1.0f), -t, glm::vec3(0.0f, 0.0f, 1.0f) * offset);
     mChangedShader.use();
   }
   else {
-    model = glm::rotate(glm::mat4(1.0f), t, glm::vec3(0.0f, 0.0f, 1.0f));
     mBasicShader.use();
   }
-  mViewMatrix = mCamera.getViewMatrix(mRenderData) * model;
+  mViewMatrix = mCamera.getViewMatrix(mRenderData);
   mUniformBuffer.uploadUboData(mViewMatrix, mProjectionMatrix);
 
   mTex.bind();
